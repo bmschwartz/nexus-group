8e636ad74288be46f79a1c41f3a9cd16918a92cf
Ben Schwartz <schwartz.ben0@gmail.com>
2021-02-03 15:29:54 -0700

    Use prisma migrate. Update prisma to 2.16.0

---
+++ prisma/migrations/20210203222614_initial/migration.sql
@@ -0,0 +1,64 @@
+-- CreateEnum
+CREATE TYPE "MembershipRole" AS ENUM ('MEMBER', 'ADMIN', 'TRADER');
+
+-- CreateEnum
+CREATE TYPE "MembershipStatus" AS ENUM ('PENDING', 'APPROVED', 'DENIED');
+
+-- CreateEnum
+CREATE TYPE "PayoutCurrency" AS ENUM ('BTC', 'ETH', 'LTC');
+
+-- CreateTable
+CREATE TABLE "Group" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+    "telegram" TEXT,
+    "discord" TEXT,
+    "email" TEXT,
+    "payoutAddress" TEXT,
+    "payoutCurrency" "PayoutCurrency" NOT NULL DEFAULT E'BTC',
+    "payInPlatform" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "GroupMembership" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "memberId" UUID NOT NULL,
+    "groupId" UUID NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT false,
+    "role" "MembershipRole" NOT NULL DEFAULT E'MEMBER',
+    "status" "MembershipStatus" NOT NULL DEFAULT E'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "GroupMembershipOption" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "groupId" UUID NOT NULL,
+    "membershipFee" DOUBLE PRECISION NOT NULL,
+    "membershipLength" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Group.name_unique" ON "Group"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "GroupMembership_memberId_groupId_key" ON "GroupMembership"("memberId", "groupId");
+
+-- AddForeignKey
+ALTER TABLE "GroupMembership" ADD FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "GroupMembershipOption" ADD FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE CASCADE ON UPDATE CASCADE;
